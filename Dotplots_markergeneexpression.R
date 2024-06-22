2a838471e2602ce72a786859f1c425a93a735d8e
kerim-a <124695518+kerim-a@users.noreply.github.com>
2024-06-22 13:39:59 +0200

    Add files via upload

--- Dotplots_markergeneexpression.R
+++ Dotplots_markergeneexpression.R
@@ -28,6 +28,7 @@ alldata_merged <- merge(g0h_filtered_cc, y = c(g24h_merged, g48h_merged, g72h_fi
 DefaultAssay(alldata_merged) <- "RNA"
 #save
 saveRDS(alldata_merged, file = "C:\\Users\\anlas\\Documents\\Phd\\RNAseq_analysis\\seurat_objects\\alldata_merged_0-72h_oldpaper_new.rds")
+alldata_merged <- readRDS(file = "C:\\Users\\anlas\\Documents\\Phd\\RNAseq_analysis\\seurat_objects\\alldata_merged_0-72h_oldpaper_new.rds")
 
 #make gene lists
 list_mesoderm <- c("T","Eomes","Gsc","Wnt3","Wnt3a","Mixl1","Snai1","Cer1","Lefty1","Meox1","Tbx6","Mesp1","Hes7","Msgn1","Foxc2","Osr1","Six2","Lhx1","Hand1","Hand2","Myl7","Pdgfra","Kdr")
@@ -35,9 +36,12 @@ list_endoderm <- c("Sox17","Foxa2","Gata4")
 list_ectoderm <- c("Sox1","Sox2","Sox3","Sox11","Ncam1","Epha5","Ptn","Prtg","Otx2","Zic1","Neurog2","Pax2","Pax6","Nes","Pou3f1","Utf1")
 list_pluripotent <- c("Zfp42","Sox2","Nanog","Esrrb","Klf4")
 list_mechanics <- c("Cdh1","Cdh2","Cdh11","Krt8","Krt18","Fn1","Vim")
+list_primedpluri <- c("Pim2","Dnmt3b","Pou3f1","Fgf5","Fst")
+
 #generate dotplots (optionally flip coordinates)
 DotPlot(alldata_merged, features = list_mesoderm, cols = c("lightgrey","dodgerblue4"), group.by = "merge.ident", scale.by = "size", scale = TRUE) #+ coord_flip()
 DotPlot(alldata_merged, features = list_endoderm, cols = c("lightgrey","dodgerblue4"), group.by = "merge.ident", scale.by = "size", scale = TRUE) #+ coord_flip()
 DotPlot(alldata_merged, features = list_pluripotent, cols = c("lightgrey","dodgerblue4"), group.by = "merge.ident", scale.by = "size", scale = TRUE) #+ coord_flip()
 DotPlot(alldata_merged, features = list_ectoderm, cols = c("lightgrey","dodgerblue4"), group.by = "merge.ident", scale.by = "size", scale = TRUE) #+ coord_flip()
 DotPlot(alldata_merged, features = list_mechanics, cols = c("lightgrey","dodgerblue4"), group.by = "merge.ident", scale.by = "size", scale = TRUE) #+ coord_flip()
+DotPlot(alldata_merged, features = list_primedpluri, cols = c("lightgrey","dodgerblue4"), group.by = "merge.ident", scale.by = "size", scale = TRUE) #+ coord_flip()
